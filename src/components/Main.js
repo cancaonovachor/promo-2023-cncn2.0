a16a49293f1efd8f80e6bbe59fb0f36774726b4f
yudaihomma <kasettoc@gmail.com>
2023-12-02 18:17:45 +0900

    フォント修正案

--- src/components/Main.js
+++ src/components/Main.js
@@ -40,15 +40,40 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">CONCEPT</h2>
-            <div class="youtube">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/XZyK2k2tf7g?si=qU1ybeh1kcflq0Bx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
-          <p>合唱団<a className="use-border" href="https://cancaonovachor.com/">CancaoNova</a>2回目の定期演奏会</p>
-          <p>私たちが選んだ珠玉の名曲と培ってきたテクノロジーの力を活用し、合唱の今と未来を描き出します。</p>
-          <p>ステージ情報は <a className="use-border" href="javascript:;" onClick={async () => {
-            await this.props.onCloseArticle();
-            this.props.onOpenArticle('stage');
-            }}>こちら</a> から
+          <div class="youtube">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/XZyK2k2tf7g?si=qU1ybeh1kcflq0Bx"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <p>
+            合唱団
+            <a className="use-border" href="https://cancaonovachor.com/">
+              CancaoNova
+            </a>
+            2回目の定期演奏会
+          </p>
+          <p>
+            私たちが選んだ珠玉の名曲と培ってきたテクノロジーの力を活用し、合唱の今と未来を描き出します。
+          </p>
+          <p>
+            ステージ情報は{' '}
+            <a
+              className="use-border"
+              href="javascript:;"
+              onClick={async () => {
+                await this.props.onCloseArticle()
+                this.props.onOpenArticle('stage')
+              }}
+            >
+              こちら
+            </a>{' '}
+            から
           </p>
           {close}
         </article>
@@ -65,145 +90,169 @@ class Main extends React.Component {
             <img src={zentai} alt="" />
           </span>
           <h3>1st Session</h3>
-          <p>支部制を活かしながら様々なコンクールに出場している我々が、
-          今年1年コンクールのために練り上げた様々な曲を、詳細な解説とあわせてお届けします。
-          ルネサンスから近現代までの「受け継いできた合唱」をご堪能ください。
+          <p>
+            支部制を活かしながら様々なコンクールに出場している我々が、
+            今年1年コンクールのために練り上げた様々な曲を、詳細な解説とあわせてお届けします。
+            ルネサンスから近現代までの「受け継いできた合唱」をご堪能ください。
           </p>
-          <p>入場料￥2500 前売り￥2000
-          <br></br>※前売り価格は2/9 23:59までです。
-          <br></br>※学生の方は全期間を通して各料金から500円引きです。
+          <p>
+            入場料￥2500 前売り￥2000
+            <br></br>※前売り価格は2/9 23:59までです。
+            <br></br>※学生の方は全期間を通して各料金から500円引きです。
           </p>
           <p>時間：13:00〜</p>
-          <p>演奏予定曲：
-          <br></br>・Kyrie (William Byrd)
-          <br></br>・Agnus dei (William Byrd)
-          <br></br>・Viel Werden Kommen von Morgen und Abend (Heinrich Schutz)
-          <br></br>・Sammlet zuvor das Unkraut (Heinrich Schutz)
-          <br></br>・Beati Mortui (Felix Mendelssohn Bartholdy)
-          <br></br>・Die Minnesänger (Robert Schumann)
-          <br></br>・Fröhlich im Maien (Richard Strauss)
-          <br></br>・Tenebrae (Marcus Ludwig)
-          <br></br>・Salut, Dame Sainte (Francis Poulenc)
-          <br></br>・Tout Puissant (Francis Poulenc)
-          <br></br>・Seigneur, je vous en prie (Francis Poulenc)
-          <br></br>・Ô mes très chers fréres (Francis Poulenc)
-          <br></br>・煙草の時II (若林千春)
-          <br></br>・金魚 (多田武彦)
-          <br></br>・The Alphabet (Ligeti György)
-          <br></br>・Khorumi (Mamia Khatelishvili)
+          <p>
+            演奏予定曲：
+            <br></br>・Kyrie (William Byrd)
+            <br></br>・Agnus dei (William Byrd)
+            <br></br>・Viel Werden Kommen von Morgen und Abend (Heinrich Schutz)
+            <br></br>・Sammlet zuvor das Unkraut (Heinrich Schutz)
+            <br></br>・Beati Mortui (Felix Mendelssohn Bartholdy)
+            <br></br>・Die Minnesänger (Robert Schumann)
+            <br></br>・Fröhlich im Maien (Richard Strauss)
+            <br></br>・Tenebrae (Marcus Ludwig)
+            <br></br>・Salut, Dame Sainte (Francis Poulenc)
+            <br></br>・Tout Puissant (Francis Poulenc)
+            <br></br>・Seigneur, je vous en prie (Francis Poulenc)
+            <br></br>・Ô mes très chers fréres (Francis Poulenc)
+            <br></br>・煙草の時II (若林千春)
+            <br></br>・金魚 (多田武彦)
+            <br></br>・The Alphabet (Ligeti György)
+            <br></br>・Khorumi (Mamia Khatelishvili)
           </p>
-          
+
           <span className="image-main main">
             <img src={secoundSession} alt="" />
           </span>
           <h3>2nd Session</h3>
-          <p>全国的な活動を通じて知り合ってきた様々なゲストとともに、今年も「今ここにある合唱」を描き出し、皆さんとともに考えます。西宮の美味しいコーヒーとチョコレート、米粉スイーツを堪能しつつ、特別ゲストとの座談会、全国各地の合唱団の演奏、パネルディスカッションなど多様な企画をお楽しみください。
+          <p>
+            全国的な活動を通じて知り合ってきた様々なゲストとともに、今年も「今ここにある合唱」を描き出し、皆さんとともに考えます。西宮の美味しいコーヒーとチョコレート、米粉スイーツを堪能しつつ、特別ゲストとの座談会、全国各地の合唱団の演奏、パネルディスカッションなど多様な企画をお楽しみください。
           </p>
-          <p>料金：無料（入場には1stまたは3rd sessionの入場券が必要です。)
-          <br></br>時間： 14:00〜</p>
-          <br/>
+          <p>
+            料金：無料（入場には1stまたは3rd sessionの入場券が必要です。)
+            <br></br>時間： 14:00〜
+          </p>
+          <br />
           <h3>合唱協賛</h3>
 
           <b>Ti Amo</b>
           <span className="image-main main">
             <img src={tiamo} alt="" />
           </span>
-          <p>アメリカ発祥のバーバーショップスタイルで演奏する、男声四人組の社会人アカペラグループ、関西を、拠点に活動中。</p>
-          <br/>
-          <b>Volare</b>
+          <p>
+            アメリカ発祥のバーバーショップスタイルで演奏する、男声四人組の社会人アカペラグループ、関西を、拠点に活動中。
+          </p>
+          <br />
+          <b>合唱団Volare</b>
           <span className="image-main main">
             <img src={volare} alt="" />
           </span>
-          <p>こんにちは！　合唱団Volareです。長田高校音楽部のOB・OG合唱団として発足して早15年。現代曲とポップスアレンジを中心に楽しく歌っています♪</p>
-          <br/>
+          <p>
+            こんにちは！　合唱団Volareです。長田高校音楽部のOB・OG合唱団として発足して早15年。現代曲とポップスアレンジを中心に楽しく歌っています♪
+          </p>
+          <br />
           <b>しゃち子。</b>
           <span className="image-main main">
             <img src={shachiko} alt="" />
           </span>
-          <p>名古屋の選りすぐりのメスゴリラを集めた女声アンサンブル。東山動物園のイケメンゴリラ、シャバーニを師と仰ぎ、迫力と美しさを兼ね備えたサウンド「ゴリリアント・ハーモニー」を目指しています。しゃち子。2度目のCNCN！メンバーも増え、さらにレベルアップした私たちをお楽しみに！</p>
-          <br/>
+          <p>
+            名古屋の選りすぐりのメスゴリラを集めた女声アンサンブル。東山動物園のイケメンゴリラ、シャバーニを師と仰ぎ、迫力と美しさを兼ね備えたサウンド「ゴリリアント・ハーモニー」を目指しています。しゃち子。2度目のCNCN！メンバーも増え、さらにレベルアップした私たちをお楽しみに！
+          </p>
+          <br />
           <b>花みずき</b>
           <span className="image-main main">
             <img src={hanamizuki} alt="" />
           </span>
-          <p>尼崎市立立花中学校合唱部の卒業生により2006年に結成。現在は、兵庫県だけでなく全国からメンバーが集い、酒井輝美の音楽観のもと、日々「花みずき」らしい音楽表現を追求している。 第4回東京国際合唱コンクール室内部門にて1位金賞および最優秀課題曲賞を受賞し、グランプリコンクールに出場。同年、作曲家Javier Busto 氏、作曲家Ambrož Čopi 氏によるレッスンを受ける。</p>
-          <br/>
+          <p>
+            尼崎市立立花中学校合唱部の卒業生により2006年に結成。現在は、兵庫県だけでなく全国からメンバーが集い、酒井輝美の音楽観のもと、日々「花みずき」らしい音楽表現を追求している。
+            第4回東京国際合唱コンクール室内部門にて1位金賞および最優秀課題曲賞を受賞し、グランプリコンクールに出場。同年、作曲家Javier
+            Busto 氏、作曲家Ambrož Čopi 氏によるレッスンを受ける。
+          </p>
+          <br />
           <b>Man de rart</b>
           <span className="image-main main">
             <img src={manderart} alt="" />
           </span>
-          <p>「王道」な男声合唱曲で滾る演奏をしたいというコンセプトのもと、2023年3月に出来た男声合唱団。
-第76回全日本合唱コンクール(大学ユースの部)にて文部科学大臣賞受賞。</p>
-          
-<br/>
+          <p>
+            「王道」な男声合唱曲で滾る演奏をしたいというコンセプトのもと、2023年3月に出来た男声合唱団。
+            第76回全日本合唱コンクール(大学ユースの部)にて文部科学大臣賞受賞。
+          </p>
+
+          <br />
           <h3>飲食協賛</h3>
           <b>ICHIJI</b>
           <span className="image-main main">
             <img src={ichiji} alt="" />
           </span>
           <p>
-          <br></br>カカオ豆本来の味わいや香りをお届けしたい。
-          <br></br>そんな思いからICHIJIは生まれました。
-          <br></br>この思いをカタチにすると
-          <br></br>チョコレートができるまでの全工程を自社工房で管理し製造する
-          <br></br>”Bean to Bar Chocolate”になりました。
-          <br></br>
-          <br></br>豆の仕入れから選別、焙煎、摩砕、調合、成形まで
-          <br></br>心をこめて手作業でやらせていただいています。
-          <br></br>
-          <br></br>きっと、チョコレートの概念が変わる
-          <br></br>”Bean to Bar Chocolate”をお楽しみください。
-          <br></br>
-          <br></br>皆さんの生活に寄り添うように楽しめて、誰かと食べると会話が生まれる。そんな世界を目指しています。
-          <br></br>
-          <br></br>
-          <b>ゆげ焙煎所</b>
-          <span className="image-main main">
-            <img src={yuge} alt="" />
-          </span>
-          <br></br>2013年、兵庫県西宮市の官公庁が多いエリアで創業。
-          <br></br>自家焙煎のスペシャルティコーヒー専門店
-          <br></br>
-          <br></br>イタリア製の完全熱風式焙煎機を使用
-          <br></br>毎朝、珈琲豆を丁寧に焙煎しています
-          <br></br>
-          <br></br>『心に響くコーヒー』
-          <br></br>
-          <br></br>を目指して、日々丁寧に
-          <br></br>取り組んでいます。
-          <br></br>
-          <br></br>
-          <b>Kinun.</b>
-          <span className="image-main main">
-            <img src={kinun} alt="" />
-          </span>
-          <br></br>生まれ育った愛知県田原市産の米粉・玄米粉を中心に
-          <br></br>生産者さんの顔のみえる食材を使いお菓子を作っています。
-          <br></br>兵庫神戸 花隈に小さなアトリエを構え、
-          <br></br>見てかわいい、聞いて安心、食べておいしい
-          <br></br>「心も体も笑顔になれる米粉sweets」をお届けしています。
+            <br></br>カカオ豆本来の味わいや香りをお届けしたい。
+            <br></br>そんな思いからICHIJIは生まれました。
+            <br></br>この思いをカタチにすると
+            <br></br>チョコレートができるまでの全工程を自社工房で管理し製造する
+            <br></br>”Bean to Bar Chocolate”になりました。
+            <br></br>
+            <br></br>豆の仕入れから選別、焙煎、摩砕、調合、成形まで
+            <br></br>心をこめて手作業でやらせていただいています。
+            <br></br>
+            <br></br>きっと、チョコレートの概念が変わる
+            <br></br>”Bean to Bar Chocolate”をお楽しみください。
+            <br></br>
+            <br></br>
+            皆さんの生活に寄り添うように楽しめて、誰かと食べると会話が生まれる。そんな世界を目指しています。
+            <br></br>
+            <br></br>
+            <b>ゆげ焙煎所</b>
+            <span className="image-main main">
+              <img src={yuge} alt="" />
+            </span>
+            <br></br>2013年、兵庫県西宮市の官公庁が多いエリアで創業。
+            <br></br>自家焙煎のスペシャルティコーヒー専門店
+            <br></br>
+            <br></br>イタリア製の完全熱風式焙煎機を使用
+            <br></br>毎朝、珈琲豆を丁寧に焙煎しています
+            <br></br>
+            <br></br>『心に響くコーヒー』
+            <br></br>
+            <br></br>を目指して、日々丁寧に
+            <br></br>取り組んでいます。
+            <br></br>
+            <br></br>
+            <b>米粉sweets kinun.</b>
+            <span className="image-main main">
+              <img src={kinun} alt="" />
+            </span>
+            <br></br>生まれ育った愛知県田原市産の米粉・玄米粉を中心に
+            <br></br>生産者さんの顔のみえる食材を使いお菓子を作っています。
+            <br></br>兵庫神戸 花隈に小さなアトリエを構え、
+            <br></br>見てかわいい、聞いて安心、食べておいしい
+            <br></br>「心も体も笑顔になれる米粉sweets」をお届けしています。
           </p>
 
           <span className="image-main main">
             <img src={kiito_senzai} alt="" />
           </span>
           <h3>3rd Session</h3>
-          <p>CancaoNovaが考える新しい合唱の形を皆様に提案します。
-          音楽とテクノロジーが融合したステージをどうぞお楽しみください。
+          <p>
+            CancaoNovaが考える新しい合唱の形を皆様に提案します。
+            音楽とテクノロジーが融合したステージをどうぞお楽しみください。
           </p>
-          <p>入場料￥2500 前売り￥2000
-          <br></br>※前売りは2/9 23:59までです。
-          <br></br>※3rd Sessionには学生割引は存在しません、ご承知おきください。
+          <p>
+            入場料￥2500 前売り￥2000
+            <br></br>※前売りは2/9 23:59までです。
+            <br></br>※3rd
+            Sessionには学生割引は存在しません、ご承知おきください。
           </p>
           <p>時間： 15:30〜</p>
-          <p> 演奏予定曲：
-          <br></br>デジタルアートステージ
-          <br></br>・Digital Poliphony (下薗大樹)
-          <br></br>・Sederunt (Perotin)
-          <br></br>・Hinbarra (Michael McGlynn)
-          <br></br>・Ut queant laxis (Orland di Lasso)
-          <br></br>・Solfeggio (Arvo Pärt)
-          <br></br>・Proverb (Steve Reich)
+          <p>
+            {' '}
+            演奏予定曲：
+            <br></br>デジタルアートステージ
+            <br></br>・Digital Poliphony (下薗大樹)
+            <br></br>・Sederunt (Perotin)
+            <br></br>・Hinbarra (Michael McGlynn)
+            <br></br>・Ut queant laxis (Orland di Lasso)
+            <br></br>・Solfeggio (Arvo Pärt)
+            <br></br>・Proverb (Steve Reich)
           </p>
           {close}
         </article>
@@ -232,17 +281,26 @@ class Main extends React.Component {
           <h2 className="major">Ticket</h2>
           <h3>入場チケット (PassMarket)</h3>
           <span className="image-main main">
-          <a href="https://passmarket.yahoo.co.jp/event/show/detail/02my1a1uzic31.html" target="_blank">
-            <img src={nyujo} alt=""  />
+            <a
+              href="https://passmarket.yahoo.co.jp/event/show/detail/02my1a1uzic31.html"
+              target="_blank"
+            >
+              <img src={nyujo} alt="" />
             </a>
           </span>
           <p>
             演奏会への入場チケットは
-          <a className="use-border" href="https://passmarket.yahoo.co.jp/event/show/detail/02my1a1uzic31.html" target="_blank">こちら</a>
-          <br/>
-          デジタルアートと合唱の融合など、新しい演奏会のスタイルを是非とも現場にてお聞き下さい。
+            <a
+              className="use-border"
+              href="https://passmarket.yahoo.co.jp/event/show/detail/02my1a1uzic31.html"
+              target="_blank"
+            >
+              こちら
+            </a>
+            <br />
+            デジタルアートと合唱の融合など、新しい演奏会のスタイルを是非とも現場にてお聞き下さい。
           </p>
-          <br/>
+          <br />
           {/* <h3>配信チケット (ZAIKO)</h3>
           <span className="image-main main">
           <a className="use-border" href="https://zaiko.io/event/354406" target="_blank">
